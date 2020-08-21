3a7a55b45b997af77d21188719095060c0c5deea
Ashish Singh <singhashish4000@gmail.com>
2020-08-21 22:28:41 +0530

    Use eip assocition and scale up

--- prod.tf
+++ prod.tf
@@ -45,6 +45,8 @@ resource "aws_security_group" "prod_web" {
 }
 
 resource "aws_instance" "prod_web" {
+  count = 2
+
   ami           = "ami-01cfa0ce6fe1024f8"
   instance_type = "t2.nano"
   key_name = "Terraform-Course"
@@ -58,8 +60,12 @@ resource "aws_instance" "prod_web" {
   }
 }
 
+resource "aws_eip_association" "prod_web" {
+  instance_id   = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
 resource "aws_eip" "prod_web" {
-  instance = aws_instance.prod_web.id
 
   tags = {
     "Teraform" : "true"
