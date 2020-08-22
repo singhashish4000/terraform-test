b84f3999418d730055fe4181da83a99cb4119ad1
Ashish Singh <singhashish4000@gmail.com>
2020-08-22 09:45:43 +0530

    commented out to test form terraform cloud ui

--- prod.tf
+++ prod.tf
@@ -36,108 +36,108 @@ resource "aws_s3_bucket" "prod_tf_course" {
   }  
 }
 
-resource "aws_default_vpc" "default" {}
+# resource "aws_default_vpc" "default" {}
 
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-west-2a"
-  tags = {
-    "Teraform" : "true"
-  }  
-}
+# resource "aws_default_subnet" "default_az1" {
+#   availability_zone = "us-west-2a"
+#   tags = {
+#     "Teraform" : "true"
+#   }  
+# }
 
-resource "aws_default_subnet" "default_az2" {
-  availability_zone = "us-west-2b"
-  tags = {
-    "Teraform" : "true"
-  }  
-}
+# resource "aws_default_subnet" "default_az2" {
+#   availability_zone = "us-west-2b"
+#   tags = {
+#     "Teraform" : "true"
+#   }  
+# }
 
-resource "aws_security_group" "prod_web" {
-  name        = "prod_web"
-  description = "Allow standard http and https ports inbound and everything outbound" 
+# resource "aws_security_group" "prod_web" {
+#   name        = "prod_web"
+#   description = "Allow standard http and https ports inbound and everything outbound" 
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.whitelist
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.whitelist
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.whitelist
-  }  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = var.whitelist
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = var.whitelist
+#   }
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "tcp"
+#     cidr_blocks = var.whitelist
+#   }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = var.whitelist
+#   }  
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = var.whitelist
+#   }
 
-  tags = {
-    "Teraform" : "true"
-  }
-}
+#   tags = {
+#     "Teraform" : "true"
+#   }
+# }
 
-resource "aws_instance" "prod_web" {
-  count = 2
+# resource "aws_instance" "prod_web" {
+#   count = 2
 
-  ami           = "ami-01cfa0ce6fe1024f8"
-  instance_type = "t2.nano"
-  key_name      = "Terraform-Course"
+#   ami           = "ami-01cfa0ce6fe1024f8"
+#   instance_type = "t2.nano"
+#   key_name      = "Terraform-Course"
 
-  vpc_security_group_ids = [
-    aws_security_group.prod_web.id 
-  ]
+#   vpc_security_group_ids = [
+#     aws_security_group.prod_web.id 
+#   ]
 
-  tags = {
-    "Teraform" : "true"
-  }
-}
+#   tags = {
+#     "Teraform" : "true"
+#   }
+# }
 
-resource "aws_eip_association" "prod_web" {
-  instance_id   = aws_instance.prod_web.0.id
-  allocation_id = aws_eip.prod_web.id
-}
+# resource "aws_eip_association" "prod_web" {
+#   instance_id   = aws_instance.prod_web.0.id
+#   allocation_id = aws_eip.prod_web.id
+# }
 
-resource "aws_eip" "prod_web" {
-  tags = {
-    "Teraform" : "true"
-  }  
-}
+# resource "aws_eip" "prod_web" {
+#   tags = {
+#     "Teraform" : "true"
+#   }  
+# }
 
-resource "aws_elb" "prod_web" {
-  name            = "prod-web"
-  instances       = aws_instance.prod_web.*.id
-  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-  security_groups = [aws_security_group.prod_web.id]
+# resource "aws_elb" "prod_web" {
+#   name            = "prod-web"
+#   instances       = aws_instance.prod_web.*.id
+#   subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+#   security_groups = [aws_security_group.prod_web.id]
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"    
-  }
-  tags = {
-    "Teraform" : "true"
-  }    
-}
+#   listener {
+#     instance_port     = 80
+#     instance_protocol = "http"
+#     lb_port           = 80
+#     lb_protocol       = "http"    
+#   }
+#   tags = {
+#     "Teraform" : "true"
+#   }    
+# }
 
-resource "aws_launch_template" "prod_web" {
-  name_prefix   = "prod-web"
-  image_id      = var.web_image_id
-  instance_type = var.web_instance_type
-  tags = {
-    "Teraform" : "true"
-  }  
-}
+# resource "aws_launch_template" "prod_web" {
+#   name_prefix   = "prod-web"
+#   image_id      = var.web_image_id
+#   instance_type = var.web_instance_type
+#   tags = {
+#     "Teraform" : "true"
+#   }  
+# }
 
 # resource "aws_autoscaling_group" "prod_web" {
 #   vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
